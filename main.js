ec1bcd40a15a58290b60ff3d0429f0ab731eea8f
Gabriel <pedromurillo96@gmail.com>
2019-08-01 10:19:28 -0700

    minor update to js file

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD2vlzUQI__PzzGTG9WQQP8d_7ik9T7PTY",
-    authDomain: "trainscheduler-83f65.firebaseapp.com",
-    databaseURL: "https://trainscheduler-83f65.firebaseio.com",
-    projectId: "trainscheduler-83f65",
-    storageBucket: "",
-    messagingSenderId: "121462351943",
-    appId: "1:121462351943:web:d0b2b9234d055a2c"
-  };
+  apiKey: "AIzaSyD2vlzUQI__PzzGTG9WQQP8d_7ik9T7PTY",
+  authDomain: "trainscheduler-83f65.firebaseapp.com",
+  databaseURL: "https://trainscheduler-83f65.firebaseio.com",
+  projectId: "trainscheduler-83f65",
+  storageBucket: "",
+  messagingSenderId: "121462351943",
+  appId: "1:121462351943:web:25327f48539a3a25"
+};
   
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
   
-  var database = firebase.database();
+var database = firebase.database();
