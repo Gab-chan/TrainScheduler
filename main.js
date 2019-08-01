3885de360b03594ccfedb30020a3392eaebefe84
Gabriel <pedromurillo96@gmail.com>
2019-08-01 09:25:40 -0700

    major updates to index.html and minor updates to other files

---
+++ main.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD2vlzUQI__PzzGTG9WQQP8d_7ik9T7PTY",
+    authDomain: "trainscheduler-83f65.firebaseapp.com",
+    databaseURL: "https://trainscheduler-83f65.firebaseio.com",
+    projectId: "trainscheduler-83f65",
+    storageBucket: "",
+    messagingSenderId: "121462351943",
+    appId: "1:121462351943:web:d0b2b9234d055a2c"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
